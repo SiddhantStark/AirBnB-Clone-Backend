75b45b24c81bc48fd7508e736188fa403e1bdaba
Siddhant Prasad <siddhantprasad.tvnl@gmail.com>
2025-06-15 22:37:56 +0530

    Minor change in UserDto

--- src/main/java/com/fullstackproject/AirBnB/dto/UserDto.java
+++ src/main/java/com/fullstackproject/AirBnB/dto/UserDto.java
@@ -1,9 +1,11 @@
 package com.fullstackproject.AirBnB.dto;
 
 import com.fullstackproject.AirBnB.entity.enums.Gender;
+import com.fullstackproject.AirBnB.entity.enums.Role;
 import lombok.Data;
 
 import java.time.LocalDate;
+import java.util.Set;
 
 @Data
 public class UserDto {
@@ -12,4 +14,5 @@ public class UserDto {
     private String name;
     private Gender gender;
     private LocalDate dateOfBirth;
+    private Set<Role> roles;
 }
